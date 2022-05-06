7593cccb6164df00fd36e29c0fe8929388409a6b
Cody Lester <kcodylester@gmail.com>
2022-05-06 17:58:41 -0400

    creating shop branch and adding home interiors

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,22 @@
 import Phaser from "phaser";
 
-import Preloader from "./scenes/Preloader";
+//import Preloader from "./scenes/Preloader";
 import Game from "./scenes/Game";
-import GameUI from "./scenes/GameUI";
+// import GameUI from "./scenes/GameUI";
 
 export default new Phaser.Game({
   type: Phaser.AUTO,
-  width: 400,
-  height: 250,
-  physics: {
-    default: "arcade",
-    arcade: {
-      gravity: { y: 0 },
-      debug: false,
-    },
-  },
-  scene: [Preloader, Game, GameUI],
-  scale: {
-    zoom: 2,
-  },
+  width: 1600,
+  height: 1600,
+  // physics: {
+  //   default: "arcade",
+  //   arcade: {
+  //     gravity: { y: 0 },
+  //     debug: false,
+  //   },
+  // },
+  scene: [Game],
+  // scale: {
+  //   zoom: 2,
+  // },
 });
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,14 +1,49 @@
-import Phaser from "phaser";
+import Phaser from "phaser"
 
 export default class Game extends Phaser.Scene {
   constructor() {
-    super("game");
+    super("game")
   }
 
-  preload() {}
+  // preload() {
+  //   this.load.image('ground', 'tiles/RPGW_Town_v1.0/ground.png' )
+  //   this.load.tilemapTiledJSON('overworld', 'tiles/overworld.json')
+  // }
 
+  // create() {
+  //   //this.add.image(0,0, 'ground')
+  //   const map = this.make.tilemap({ key: 'overworld' });
+  //   const map.addTilesetImage("houses");
+
+  // }
+  preload() {
+    this.load.image('houses', 'tiles/RPGW_Town_v1.0/houses.png');
+    this.load.image('outside', 'tiles/RPGW_Town_v1.0/outside.png');
+    this.load.image('player', 'NPC_CHaracters_v1/Male1.png');
+    this.load.tilemapTiledJSON('overworld', 'tiles/overworld.json');
+  }
   create() {
-    const map = this.make.tilemap({ key: "overworld" });
-    map.addTilesetImage("houses");
+
+    const map = this.make.tilemap({ key:'overworld' });
+    const townTileSet = map.addTilesetImage('Town', 'outside');
+    const houseTileSet = map.addTilesetImage('Houses', 'houses');
+    //const player = this.physics.add.sprite(10, 20, 'player', )
+    
+    map.createLayer('Ground', townTileSet);
+    const housesLayer = map.createLayer('Houses', houseTileSet);
+    const wallsLayer = map.createLayer('Walls', townTileSet);
+    wallsLayer.setCollisionByProperty({ collides: true })
+    housesLayer.setCollisionByProperty({ collides: true })
+
+    // const debugDraw = (layer: Phaser.Tilemaps.TilemapLayer, scene: Phaser.Scene) => {
+    //   const debugGraphics = scene.add.graphics().setAlpha(0.7)
+    //   layer.renderDebug(debugGraphics, {
+    //     tileColor: null,
+    //     collidingTileColor: new Phaser.Display.Color(243, 234, 48, 255),
+    //     faceColor: new Phaser.Display.Color(40, 39, 37, 255)
+    //   })
+    // }
+    //map.create
   }
 }
+
