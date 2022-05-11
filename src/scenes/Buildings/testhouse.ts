48789c9bcfbbc949311f4ad77cfd887dd0e85457
Parry <pt987@nyu.edu>
2022-05-11 10:13:25 -0400

    added scene changes

---
+++ src/scenes/Buildings/testhouse.ts
@@ -0,0 +1,13 @@
+import Phaser from "phaser";
+
+export default class testhouse extends Phaser.Scene {
+  constructor() {
+    super("game");
+  }
+
+  preload() {
+    //Load graphics
+  }
+
+  update() {}
+}
